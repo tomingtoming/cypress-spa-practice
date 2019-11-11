e7f4b11e76de67b395996d0375bb8c11495f792f
toming <tomingtoming@gmail.com>
2019-11-12 07:20:43 +0900

    simple web counter spa

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios, {AxiosResponse} from 'axios'
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface error {
+    message: string;
+}
+
+function App() {
+    const [hit, setHit] = React.useState<number | error>();
+    React.useEffect(() => {
+        axios.get("https://api.countapi.xyz/hit/tomingtoming/cypress-spa-practice")
+            .then((res: AxiosResponse<{ value: number }>) => setHit(res.data.value))
+            .catch(e => setHit({message: 'something error happened'}));
+    }, [setHit]);
+    return (
+        <div className="App">
+            {typeof hit === 'number' && <>Counter: {hit}</>}
+            {typeof hit === 'object' && <>Counter: ERROR[{hit.message}]</>}
+            {hit === undefined && <>Counter: Loading...</>}
+        </div>
+    );
 }
 
 export default App;
